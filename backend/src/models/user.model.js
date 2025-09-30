6b61fd3021a8429d38106773020eff2a5de3273f
Mubaraq Allamalyekeen <mubaraqadeniyi159@gmail.com>
2025-09-30 11:09:38 +0100

    record dashboard

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
       required: [true, "password is required"],
       minlength: [6, "Password must be atleast 6 characters long"],
     },
+    isPregnant: { type: Boolean, default: true },
+    EDD: { type: Date },
+
+    //EDD means (expected date of delivery)
   },
   { timestamps: true }
 );
